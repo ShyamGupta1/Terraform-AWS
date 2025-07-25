c725637d08beff3601fa2a8f3ffaf41424ba28dc
Karl_X_ <110926625+ShyamGupta1@users.noreply.github.com>
2025-07-26 02:16:08 +0530

    Fix missing default export in Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,1 +1,12 @@
-// This will show us the current structure
+import React from 'react';
+
+const Index = () => {
+  return (
+    <div>
+      <h1>Index Page</h1>
+      <p>This is the main index page.</p>
+    </div>
+  );
+};
+
+export default Index;
